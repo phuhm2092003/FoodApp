8fd7cfa35610dbcc9164b02f195933f912494a90
DELL <hominphuzxc@gmail.com>
2023-03-21 23:21:12 +0700

    Done SplashActivity.java

--- app/src/main/java/fpt/edu/foodlyapplication/view/SplashActivity.java
+++ app/src/main/java/fpt/edu/foodlyapplication/view/SplashActivity.java
@@ -2,15 +2,27 @@ package fpt.edu.foodlyapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import fpt.edu.foodlyapplication.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    public static final int SCREEN_TIME_OUT = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, SignInActivity.class));
+            }
+        }, SCREEN_TIME_OUT);
     }
 }
