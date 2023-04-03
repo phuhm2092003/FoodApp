0ccad577d235c5e07b12d7820b0cdf3fb76b3228
DELL <hominphuzxc@gmail.com>
2023-04-03 22:45:47 +0700

    Clean Splah Screen

--- app/src/main/java/fpt/edu/foodlyapplication/view/SplashActivity.java
+++ app/src/main/java/fpt/edu/foodlyapplication/view/SplashActivity.java
@@ -16,10 +16,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        // Navigate to SignInActivity after a delay
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashActivity.this, SignInActivity.class));
