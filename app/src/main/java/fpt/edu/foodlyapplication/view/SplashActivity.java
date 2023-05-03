943d1cfba0f9fe3b92b38537fc5a21e49d1b0cd2
DELL <hominphuzxc@gmail.com>
2023-05-03 13:02:36 +0700

    Clean SplashActivity.java

--- app/src/main/java/fpt/edu/foodlyapplication/view/SplashActivity.java
+++ app/src/main/java/fpt/edu/foodlyapplication/view/SplashActivity.java
@@ -2,25 +2,28 @@ package fpt.edu.foodlyapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 import fpt.edu.foodlyapplication.R;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
-    public static final int SCREEN_TIMEOUT = 2000;
+    public static final int TIME_DELAY = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, SignInActivity.class));
-            }
-        }, SCREEN_TIMEOUT);
+
+        new Handler().postDelayed(() -> startSignInScreen(), TIME_DELAY);
+    }
+
+    private void startSignInScreen() {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
     }
 }
